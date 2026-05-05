a2350507f98a14b55e27af7f7802fa532e01998b
ASR-CuriousOne <stark.the.avenger.2006@gmail.com>
2026-05-06 00:35:53 +0530

    Build: Changed build system to build seperate release and debug configs.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,20 @@
-mkdir -p build
-cmake -B build -G "Unix Makefiles"
-cmake --build build
+#!/bin/bash
+
+BUILD_TYPE="Debug"
+BUILD_DIR="build/debug"
+
+if [ "$1" == "release" ] || [ "$1" == "Release" ]; then
+    BUILD_TYPE="Release"
+    BUILD_DIR="build/release"
+    echo "--- Configuring for Release Mode ---"
+else
+    echo "--- Configuring for Debug Mode ---"
+fi
+
+mkdir -p $BUILD_DIR
+
+cmake -B $BUILD_DIR -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake --build $BUILD_DIR
 
 echo Compiling Shaders....
 glslc shaders/vert.vert -o shaders/vert.spv
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-ln -s ./build/compile_commands.json compile_commands.json
-ln -s ./build/bin/GhostRenderApp app.out
+ln -s ./build/debug/compile_commands.json compile_commands.json
+ln -s ./build/release/bin/GhostRenderApp appRelease.out
+ln -s ./build/debug/bin/GhostRenderApp appDebug.out
 
